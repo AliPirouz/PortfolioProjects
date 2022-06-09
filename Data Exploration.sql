72dc5a229092de49a224878e84568977c7ac49db
Ali Pirouz <91910076+AliPirouz@users.noreply.github.com>
2022-06-09 19:32:45 -0400

    Update Data Exploration.sql

--- Data Exploration.sql
+++ Data Exploration.sql
@@ -6,6 +6,7 @@ https://ourworldindata.org/covid-deaths
 Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
 */
 
+--------------------------------------------------------------------------------------------------------------------------
 
 -- Select Data that we are going to work with
 
@@ -14,6 +15,7 @@ From CovidDeaths
 Where continent is not null 
 order by 1,2
 
+--------------------------------------------------------------------------------------------------------------------------
 
 -- Total Cases vs Total Deaths
 -- (Shows the likelihood of dying if you contract covid in your country)
@@ -24,6 +26,7 @@ Where location like ‘%canada%’
 and continent is not null 
 order by 1,2
 
+--------------------------------------------------------------------------------------------------------------------------
 
 -- Total Cases vs Population
 -- (Shows what percentage of population infected with Covid)
@@ -32,6 +35,7 @@ Select Location, date, Population, total_cases,  (total_cases/population) *100 a
 From CovidDeaths
 order by 1,2
 
+--------------------------------------------------------------------------------------------------------------------------
 
 -- Countries with Highest Infection Rate compared to Population
 
@@ -40,6 +44,7 @@ From CovidDeaths
 Group by Location, Population
 order by PercentPopulationInfected desc
 
+--------------------------------------------------------------------------------------------------------------------------
 
 -- Countries with Highest Death Count per Population
 
@@ -49,6 +54,7 @@ Where continent is not null
 Group by Location
 order by TotalDeathCount desc
 
+--------------------------------------------------------------------------------------------------------------------------
 
 -- Breaking things down by continent
 -- (Showing continents with the highest death count per population)
@@ -59,6 +65,7 @@ Where continent is not null
 Group by continent
 order by TotalDeathCount desc
 
+--------------------------------------------------------------------------------------------------------------------------
 
 -- Global Numbers
 
@@ -67,6 +74,7 @@ From CovidDeaths
 where continent is not null 
 order by 1,2
 
+--------------------------------------------------------------------------------------------------------------------------
 
 -- Total Population vs Vaccinations
 -- (Shows Percentage of population that has received at least one dose of covid vaccine)
@@ -80,6 +88,7 @@ Join CovidVax vac
 where dea.continent is not null 
 order by 2,3
 
+--------------------------------------------------------------------------------------------------------------------------
 
 -- Using CTE to perform calculation on “partition by” in previous query
 
@@ -99,7 +108,7 @@ where dea.continent is not null
 Select *, (RollingPeopleVaccinated/Population) *100
 From PopvsVac
 
-
+--------------------------------------------------------------------------------------------------------------------------
 
 -- Using “Temp Table” to perform calculation on “Partition By” in previous query
 
@@ -128,8 +137,7 @@ Join CovidVax vac
 Select *, (RollingPeopleVaccinated/Population)*100
 From #PercentPopulationVaccinated
 
-
-
+--------------------------------------------------------------------------------------------------------------------------
 
 -- Creating View to store data for later visualizations
 
@@ -142,4 +150,3 @@ Join CovidVax vac
 	On dea.location = vac.location
 	and dea.date = vac.date
 where dea.continent is not null 
-
